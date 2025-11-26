a0cda474e51f97c1bab13ba494ffa98b83c48aa7
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-11-25 19:31:11 -0600

    Implement test for index scan in query
    Add test for index scan in detalle_pedido query

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,1 +1,5 @@
+from db import run_query
 
+def test_index_cliente_producto():
+    result = run_query("EXPLAIN SELECT * FROM detalle_pedido WHERE id_pedido=1 AND id_producto=2;")
+    assert any("Index Scan" in str(r) for r in result)
