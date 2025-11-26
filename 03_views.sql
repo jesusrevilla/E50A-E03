1c9fbdf0297e8c9d64e77890e4a118c818566ef7
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-25 19:11:11 -0600

    Create 03_views.sql

---
+++ 03_views.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE VIEW vista_detalle_pedidos AS
+SELECT 
+    p.id_pedido,
+    c.nombre AS cliente,
+    pr.nombre AS producto,
+    dp.cantidad,
+    pr.precio,
+    dp.cantidad * pr.precio AS total_linea
+FROM pedidos p
+JOIN clientes c ON p.id_cliente = c.id_cliente
+JOIN detalle_pedido dp ON p.id_pedido = dp.id_pedido
+JOIN productos pr ON dp.id_producto = pr.id_producto;
