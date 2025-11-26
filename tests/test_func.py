aa652638c0384f774a45de7278664bbf7f413ac8
David Vidales <vidalesrmzdavid@gmail.com>
2025-11-25 19:45:26 -0600

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,1 +1,23 @@
+import psycopg2
+import pytest
 
+DB_CONFIG = {
+    "dbname": "test_db",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": 5432
+}
+
+def run_query(q):
+    with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(q)
+            try:
+                return cur.fetchall()
+            except:
+                return None
+
+def test_total_gastado():
+    result = run_query("SELECT total_gastado_por_cliente(1);")
+    assert result[0][0] > 0
