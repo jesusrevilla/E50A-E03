8dec697b9de1784094c86569f05b0ce5971d87a2
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:34:26 -0600

    Update test_store_proc.py

--- tests/test_store_proc.py
+++ tests/test_store_proc.py
@@ -14,3 +14,24 @@ def run_query(query):
         with conn.cursor() as cur:
             cur.execute(query)
             return cur.fetchall()
+
+def test_registrar_pedido():
+    query = """
+        CALL registrar_pedido(1, '2025-05-10', 3, 2);
+    """
+    run_query(query)
+    
+    query_pedido = """
+        SELECT * FROM pedidos WHERE id_cliente = 1 AND fecha = '2025-05-10';
+    """
+    pedido_result = run_query(query_pedido)
+
+    query_detalle = """
+        SELECT * FROM detalle_pedido WHERE id_pedido = %s;
+    """
+    detalle_result = run_query(query_detalle % (pedido_result[0][0],))
+
+    expected_detalle = [(pedido_result[0][0], 3, 2)]  
+
+    assert len(pedido_result) == 1
+    assert detalle_result == expected_detalle
