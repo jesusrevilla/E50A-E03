34af1edf99793790f6f11aa9eb8dcf5d0e3a879f
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:19:44 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -36,3 +36,14 @@ INSERT INTO usuarios (nombre, correo, historial_actividad) VALUES
     {"fecha": "2025-05-04", "accion": "comentó_publicación"}
 ]');
 
+-- Ciudades
+INSERT INTO ciudades (nombre) VALUES
+('San Luis Potosí'), ('Querétaro'), ('Guadalajara'), ('Monterrey'), ('CDMX');
+
+-- Rutas (grafo dirigido)
+INSERT INTO rutas (id_origen, id_destino, distancia_km) VALUES
+(1, 2, 180),  -- SLP → Querétaro
+(2, 3, 350),  -- Querétaro → Guadalajara
+(1, 5, 410),  -- SLP → CDMX
+(5, 4, 900),  -- CDMX → Monterrey
+(3, 4, 700);  -- Guadalajara → Monterrey
