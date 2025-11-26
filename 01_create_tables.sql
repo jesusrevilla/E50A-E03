b7543d842bcf83eedab74f0f34f79aa407f07655
tacodepastor25 <125671857+tacodepastor25@users.noreply.github.com>
2025-11-25 19:56:20 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,67 +1,58 @@
-
--- 1 Tablas Relacionales 
-
-CREATE TABLE clientes (
+-- 1. Tablas Base
+CREATE TABLE IF NOT EXISTS clientes (
     id_cliente SERIAL PRIMARY KEY,
     nombre VARCHAR(100),
     correo VARCHAR(100)
 );
 
-CREATE TABLE productos (
+CREATE TABLE IF NOT EXISTS productos (
     id_producto SERIAL PRIMARY KEY,
     nombre VARCHAR(100),
     precio DECIMAL(10, 2)
 );
 
-CREATE TABLE pedidos (
+CREATE TABLE IF NOT EXISTS pedidos (
     id_pedido SERIAL PRIMARY KEY,
     id_cliente INT REFERENCES clientes(id_cliente),
     fecha DATE
 );
 
-CREATE TABLE detalle_pedido (
+CREATE TABLE IF NOT EXISTS detalle_pedido (
     id_detalle SERIAL PRIMARY KEY,
     id_pedido INT REFERENCES pedidos(id_pedido),
     id_producto INT REFERENCES productos(id_producto),
     cantidad INT
 );
 
--- Tabla para el Trigger 
-CREATE TABLE auditoria_pedidos (
+-- 4. Tabla Auditoría
+CREATE TABLE IF NOT EXISTS auditoria_pedidos (
     id_auditoria SERIAL PRIMARY KEY,
     id_cliente INT,
     fecha_pedido DATE,
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Indice solicitado 
-CREATE INDEX idx_cliente_producto ON detalle_pedido(id_pedido, id_producto);
-
-
--- 2 Tablas NoSQL , JSON 
-
-CREATE TABLE productos_json (
+-- 5. Tablas JSONB
+CREATE TABLE IF NOT EXISTS productos_json (
     id SERIAL PRIMARY KEY,
     nombre TEXT,
     atributos JSONB
 );
 
-CREATE TABLE usuarios (
+CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
     nombre TEXT,
     correo TEXT,
     historial_actividad JSONB
 );
 
-
--- 3 Tablas Grafos 
-
-CREATE TABLE ciudades (
+-- 6. Tablas Grafos
+CREATE TABLE IF NOT EXISTS ciudades (
     id SERIAL PRIMARY KEY,
     nombre TEXT NOT NULL
 );
 
-CREATE TABLE rutas (
+CREATE TABLE IF NOT EXISTS rutas (
     id_origen INT REFERENCES ciudades(id),
     id_destino INT REFERENCES ciudades(id),
     distancia_km INT,
