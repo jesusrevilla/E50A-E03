7d953ac10bd97f9d174ccd3be6b29c6a7089a510
menaxixi <179800@upslp.edu.mx>
2025-11-25 19:30:08 -0600

    Update 03_retrieve_everything_from_a_table.sql

--- 03_retrieve_everything_from_a_table.sql
+++ 03_retrieve_everything_from_a_table.sql
@@ -1,4 +1,4 @@
---ejercicio de procedimiento
+--vista
 CREATE OR REPLACE VIEW vista_detalle_pedidos AS
 SELECT 
     p.id_pedido,
@@ -13,4 +13,25 @@ JOIN detalle_pedido d ON p.id_pedido = d.id_pedido
 JOIN productos pr   ON d.id_producto = pr.id_producto;
 
 CALL registrar_pedido(1, '2025-05-20', 2, 3);
---
+
+--ejercicio de procedimiento
+CREATE OR REPLACE PROCEDURE registrar_pedido(
+    _id_cliente INT,
+    _fecha DATE,
+    _id_producto INT,
+    _cantidad INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    nuevo_pedido INT;
+BEGIN
+    INSERT INTO pedidos (id_cliente, fecha)
+    VALUES (_id_cliente, _fecha)
+    RETURNING id_pedido INTO nuevo_pedido;
+
+    INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad)
+    VALUES (nuevo_pedido, _id_producto, _cantidad);
+END;
+$$;
+
