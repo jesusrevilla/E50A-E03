72969110a1ffa1be44ef04f11960248f9ff8b382
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-11-25 19:45:39 -0600

    Update test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,5 +1,5 @@
 import psycopg2
-from .conftest import db_connection # Asume configuración de conexión
+from .conftest import db_connection
 
 def test_consulta_productos_por_marca():
     """Verifica la consulta con el operador ->> en la tabla productos_json."""
@@ -20,7 +20,6 @@ def test_consulta_usuarios_por_accion():
     conn = db_connection()
     cur = conn.cursor()
     
-    # Debe encontrar a Laura Gómez y Pedro Ruiz
     cur.execute("SELECT COUNT(*) FROM usuarios WHERE historial_actividad @> '[{\"accion\": \"comentó_publicación\"}]';")
     count = cur.fetchone()[0]
     
