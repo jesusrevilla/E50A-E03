09657bba2d9d624d0310f3b23a74f00756a5e02b
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-25 19:20:24 -0600

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,1 +1,11 @@
+import psycopg2
+
+def test_total_gastado_por_cliente():
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost")
+    cur = conn.cursor()
+
+    cur.execute("SELECT total_gastado_por_cliente(1);")
+    total = cur.fetchone()[0]
+
+    assert total >= 0
 
