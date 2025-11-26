64c0eabf0c96e7a51b6fda9198090ef7d26a1e0d
David Vidales <vidalesrmzdavid@gmail.com>
2025-11-25 19:39:33 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,1 +1,30 @@
+import psycopg2
+import pytest
 
+DB_CONFIG = {
+    "dbname": "test_db",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": 5432
+}
+
+def run_query(q):
+    with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(q)
+            try:
+                return cur.fetchall()
+            except:
+                return None
+
+# ====================================================
+# TEST VISTA
+# ====================================================
+
+def test_vista_detalle():
+    result = run_query("SELECT cliente, producto, cantidad FROM vista_detalle_pedidos ORDER BY id_pedido, id_producto;")
+    assert ("Ana Torres", "Laptop", 1) in result
+    assert ("Ana Torres", "Mouse", 2) in result
+    assert ("Luis Pérez", "Teclado", 1) in result
+    assert len(result) == 3
