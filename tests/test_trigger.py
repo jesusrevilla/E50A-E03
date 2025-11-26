4d0cd8994f687a788d14ea61c3ee47a6daa82010
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-25 19:53:30 -0600

    Update test_trigger.py

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -1,1 +1,12 @@
+def test_trigger_auditoria(trigger_conn):
+    cur = trigger_conn.cursor()
+
+    # Insertar un nuevo pedido
+    cur.execute("INSERT INTO pedidos (id_cliente, fecha) VALUES (1, '2025-05-22');")
+
+    # Comprobar auditoría
+    cur.execute("SELECT * FROM auditoria_pedidos WHERE fecha_pedido='2025-05-22';")
+    rows = cur.fetchall()
+
+    assert len(rows) == 1
 
