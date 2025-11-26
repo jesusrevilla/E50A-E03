4efea2f68300b13c6eea5b315b89d310a8cc43f4
miritzatorres8-lab <miritzatorres8@gmail.com>
2025-11-26 14:54:14 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,13 @@
+SELECT * FROM clientes;
+SELECT * FROM productos;
+SELECT * FROM pedidos;
+SELECT * FROM detalle_pedido;
+SELECT * FROM vista_detalle_pedidos;
+SELECT total_gastado_por_cliente(1);
+CALL registrar_pedido(1, '2025-05-20', 2, 3);
+SELECT * FROM auditoria_pedidos;
+SELECT * FROM productos_json;
+SELECT * FROM usuarios;
+SELECT * FROM ciudades;
+SELECT * FROM rutas;
 
