69375181ab3a9ee32102a19d1c222562b7fda528
menaxixi <179800@upslp.edu.mx>
2025-11-25 19:57:55 -0600

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -2,35 +2,17 @@ import psycopg2
 
 def test_index_exists():
     conn = psycopg2.connect(
-        dbname='test_db',
-        user='postgres',
-        password='postgres',
-        host='localhost',
-        port=5432
+        dbname="test_db", user="postgres", password="postgres", host="localhost", port="5432"
     )
     cur = conn.cursor()
 
     cur.execute("""
         SELECT indexname
         FROM pg_indexes
-        WHERE tablename = 'clientes';
+        WHERE tablename='detalle_pedido';
     """)
-    indexes = [r[0] for r in cur.fetchall()]
+    indices = [i[0] for i in cur.fetchall()]
 
-    assert 'idx_clientes_nombre' in indexes
+    assert "idx_cliente_producto" in indices
 
-
-def test_index_used_in_query():
-    conn = psycopg2.connect(
-        dbname='test_db',
-        user='postgres',
-        password='postgres',
-        host='localhost',
-        port=5432
-    )
-    cur = conn.cursor()
-
-    cur.execute("EXPLAIN SELECT * FROM clientes WHERE nombre = 'juan';")
-    plan = "\n".join(row[0] for row in cur.fetchall())
-
-    assert "Index Scan" in plan or "Bitmap Index Scan" in plan
+    conn.close()
