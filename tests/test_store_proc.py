f99bde95ea2a0242d759d2165501edb4319aadda
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-25 19:21:12 -0600

    Update test_store_proc.py

--- tests/test_store_proc.py
+++ tests/test_store_proc.py
@@ -1,1 +1,14 @@
+import psycopg2
+
+def test_registrar_pedido():
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost")
+    cur = conn.cursor()
+
+    cur.execute("CALL registrar_pedido(1, '2025-05-20', 2, 3);")
+    conn.commit()
+
+    cur.execute("SELECT COUNT(*) FROM pedidos WHERE fecha='2025-05-20';")
+    cantidad = cur.fetchone()[0]
+
+    assert cantidad >= 1
 
