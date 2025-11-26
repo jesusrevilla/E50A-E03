727a1c58045a73bf178541002425106985a1e731
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:14:09 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -24,3 +24,9 @@ CREATE TABLE detalle_pedido (
     cantidad INT
 );
 
+CREATE TABLE auditoria_pedidos (
+    id_auditoria SERIAL PRIMARY KEY,
+    id_cliente INT,
+    fecha_pedido DATE,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
