ac29a708dfdd5a1593608c2c366153bfa81f92f1
Fano-Rdz <178584@upslp.edu.mx>
2025-11-25 19:36:40 -0600

    Update test_store_proc.py

--- tests/test_store_proc.py
+++ tests/test_store_proc.py
@@ -4,8 +4,8 @@ def test_registrar_pedido():
     # Inserta un pedido
     run_query("CALL registrar_pedido(1, '2025-05-20', 2, 3);")
     result = run_query("SELECT * FROM detalle_pedido WHERE id_pedido = (SELECT MAX(id_pedido) FROM pedidos);")
-    assert result[0][2] == 2  # id_producto
-    assert result[0][3] == 3  # cantidad
+    assert result[0][2] == 2 
+    assert result[0][3] == 3
 
 def test_total_gastado():
     result = run_query("SELECT total_gastado_por_cliente(1);")
