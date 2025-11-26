966574bf8776c9de0c49191aafa9f74f94bc5d63
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-11-25 19:52:14 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -20,4 +20,7 @@ INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad) VALUES
 (1, 2, 2),  -- Ana compra 2 Mouse
 (2, 3, 1);  -- Luis compra 1 Teclado
 
-
+INSERT INTO productos_json (nombre, atributos) VALUES
+('Laptop', '{"marca": "Dell", "ram": "16GB", "procesador": "Intel i7"}'),
+('Smartphone', '{"marca": "Samsung", "pantalla": "6.5 pulgadas", "almacenamiento": "128GB"}'),
+('Tablet', '{"marca": "Apple", "modelo": "iPad Air", "color": "gris"}');
