7a3f45c32778bd0972b1f1acacfcb2f719c72dee
Rafael Sánchez Saucedo <rafaelsanchez9924@gmail.com>
2025-11-25 19:54:03 -0600

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,1 +1,7 @@
+def test_tablas_existen(index_conn):
+    cur = index_conn.cursor()
+
+    for table in ["clientes", "productos", "pedidos", "detalle_pedido"]:
+        cur.execute(f"SELECT to_regclass('{table}');")
+        assert cur.fetchone()[0] == table
 
