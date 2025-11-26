f526418bfddb64d940bb3d5ec04ae371a9d03bf6
EDLR <160347985+ErikDlrr@users.noreply.github.com>
2025-11-25 19:18:10 -0600

    Add test for index existence in test_index.py
    Add a test to verify the existence of the 'idx_cliente_producto' index on the 'detalle_pedido' table

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,1 +1,16 @@
+import psycopg2
+import pytest
 
+def test_index_exists():
+    conn = psycopg2.connect(
+        dbname="test_db", user="postgres", password="postgres", host="localhost", port="5432"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT indexname FROM pg_indexes WHERE tablename = 'detalle_pedido' AND indexname = 'idx_cliente_producto';")
+    res = cur.fetchone()
+    
+    assert res is not None
+    assert res[0] == 'idx_cliente_producto'
+    
+    cur.close()
+    conn.close()
