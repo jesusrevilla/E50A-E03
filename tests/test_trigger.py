fc481f5f32d66749feac7918d5da9d2a872f71ff
patymtz17 <181730@upslp.edu.mx>
2025-11-25 19:36:58 -0600

    Update test_trigger.py

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -1,1 +1,9 @@
+def test_trigger(conn):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO pedidos(id_cliente, fecha) VALUES (1, '2025-05-21');")
+    conn.commit()
+
+    cur.execute("SELECT * FROM auditoria_pedidos ORDER BY id_auditoria DESC LIMIT 1;")
+    row = cur.fetchone()
+    assert row is not None
 
