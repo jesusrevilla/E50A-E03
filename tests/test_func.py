81bfb029dbac2f98993b9c62f52e12b98bd68b11
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-26 02:31:29 -0600

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,1 +1,16 @@
+import pytest
+import psycopg2
 
+def test_total_gastado_por_cliente():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="exercises",
+        user="postgres",
+        password="postgres"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT total_gastado_por_cliente(1)")
+    total = cur.fetchone()[0]
+    assert total > 0
+    cur.close()
+    conn.close()
