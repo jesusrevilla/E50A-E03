a81b9fd9888948b6d60e6f244519d1056fa5e79e
YaelQCI <181914@upslp.edu.mx>
2025-11-25 19:22:23 -0600

    Insert new cities and routes into the database

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -38,3 +38,17 @@ INSERT INTO usuarios (nombre, correo, historial_actividad) VALUES
     {"fecha": "2025-05-01", "accion": "inicio_sesion"},
     {"fecha": "2025-05-04", "accion": "comentó_publicación"}
 ]');
+
+INSERT INTO ciudades (nombre) VALUES
+('San Luis Potosí'), 
+('Querétaro'),       
+('Guadalajara'),     
+('Monterrey'),       
+('CDMX');           
+
+INSERT INTO rutas (id_origen, id_destino, distancia_km) VALUES
+(1, 2, 180),  
+(2, 3, 350),  
+(1, 5, 410),  
+(5, 4, 900), 
+(3, 4, 700);  
