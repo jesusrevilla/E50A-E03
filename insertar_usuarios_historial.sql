9d6e9b5fae525a5d0c8024e4bef05c5aca7ef3d5
Karina123Kma <upslpbd@gmail.com>
2025-11-25 19:32:50 -0600

    Create insertar_usuarios_historial.sql

---
+++ insertar_usuarios_historial.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuarios (nombre, correo, historial_actividad) VALUES
+('Laura Gómez', 'laura@example.com', '[{"fecha": "2025-05-01", "accion": "inicio_sesion"}, {"fecha": "2025-05-02", "accion": "subio_archivo"}, {"fecha": "2025-05-03", "accion": "cerró_sesion"}]'),
+('Pedro Ruiz', 'pedro@example.com', '[{"fecha": "2025-05-01", "accion": "inicio_sesion"}, {"fecha": "2025-05-04", "accion": "comentó_publicación"}]');
