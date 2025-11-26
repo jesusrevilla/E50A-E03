bc360bb44c67ff7c99c67bf8fc2fa2401ca24ed1
Jpablo179804 <179804@upslp.edu.mx>
2025-11-25 19:52:56 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,24 +1,29 @@
 import psycopg2
 import pytest
 
-def test_vista_detalle_pedidos():
-    conn = psycopg2.connect(
+def get_connection():
+    return psycopg2.connect(
         host="localhost",
-        database="exercises",
+        database="exercises", 
         user="postgres",
         password="postgres"
     )
+
+def test_vista_detalle_pedidos():
+    conn = get_connection()
     cur = conn.cursor()
     
-    cur.execute("""
-        SELECT COUNT(*) FROM information_schema.views 
-        WHERE table_name = 'vista_detalle_pedidos'
-    """)
-    assert cur.fetchone()[0] == 1
-    
-    cur.execute("SELECT COUNT(*) FROM vista_detalle_pedidos")
-    count = cur.fetchone()[0]
-    assert count >= 3  
-    
-    cur.close()
-    conn.close()
+    try:
+        cur.execute("""
+            SELECT COUNT(*) FROM information_schema.views 
+            WHERE table_name = 'vista_detalle_pedidos'
+        """)
+        assert cur.fetchone()[0] == 1
+        
+        cur.execute("SELECT COUNT(*) FROM vista_detalle_pedidos")
+        count = cur.fetchone()[0]
+        assert count >= 3
+        
+    finally:
+        cur.close()
+        conn.close()
