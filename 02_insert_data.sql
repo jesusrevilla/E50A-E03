187d6e6bf4a4999bac5fd6db3e83ca059bb64680
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-26 00:34:31 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,8 +1,6 @@
-
 INSERT INTO clientes (nombre, correo) VALUES
 ('Ana Torres', 'ana@example.com'),
 ('Luis Pérez', 'luis@example.com');
-
 
 INSERT INTO productos (nombre, precio) VALUES
 ('Laptop', 1200.00),
@@ -14,9 +12,9 @@ INSERT INTO pedidos (id_cliente, fecha) VALUES
 (2, '2025-05-02');
 
 INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad) VALUES
-(1, 1, 1),  
-(1, 2, 2), 
-(2, 3, 1); 
+(1, 1, 1),
+(1, 2, 2),
+(2, 3, 1);
 
 INSERT INTO productos_json (nombre, atributos) VALUES
 ('Laptop', '{"marca": "Dell", "ram": "16GB", "procesador": "Intel i7"}'),
@@ -37,10 +35,9 @@ INSERT INTO usuarios (nombre, correo, historial_actividad) VALUES
 INSERT INTO ciudades (nombre) VALUES
 ('San Luis Potosí'), ('Querétaro'), ('Guadalajara'), ('Monterrey'), ('CDMX');
 
-
 INSERT INTO rutas (id_origen, id_destino, distancia_km) VALUES
-(1, 2, 180),  
-(2, 3, 350),  
-(1, 5, 410),  
-(5, 4, 900), 
-(3, 4, 700);  
+(1, 2, 180),
+(2, 3, 350),
+(1, 5, 410),
+(5, 4, 900),
+(3, 4, 700);
