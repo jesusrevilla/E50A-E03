09752b4f8c5c467410f14f208673f4b349efbcf9
Uriel4443 <182829@upslp.edu.mx>
2025-11-25 19:48:58 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,14 +1,6 @@
 CREATE VIEW vista_detalle_pedidos AS
-SELECT 
-    p.id_pedido,
-    c.nombre AS cliente,
-    pr.nombre AS producto,
-    d.cantidad,
-    pr.precio,
-    (d.cantidad * pr.precio) AS total_linea
-FROM detalle_pedido d
-JOIN pedidos p ON d.id_pedido = p.id_pedido
-JOIN clientes c ON p.id_cliente = c.id_cliente
+SELECT p.id_pedido,c.nombre AS cliente,pr.nombre AS producto,d.cantidad,pr.precio,(d.cantidad * pr.precio) AS total_linea
+FROM detalle_pedido d JOIN pedidos p ON d.id_pedido = p.id_pedido JOIN clientes c ON p.id_cliente = c.id_cliente
 JOIN productos pr ON d.id_producto = pr.id_producto;
 
 CREATE OR REPLACE PROCEDURE registrar_pedido(id_cliente INTEGER,fecha DATE,id_producto INTEGER,cantidad INTEGER)
