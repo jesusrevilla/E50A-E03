728a4a91cc28d92a929e0fb9c3ffa92a2ed2f166
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:33:04 -0600

    Update test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,1 +1,16 @@
+import psycopg2
+import pytest
 
+DB_CONFIG = {
+    "dbname": "test_db",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": 5432
+}
+
+def run_query(query):
+    with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+            return cur.fetchall()
