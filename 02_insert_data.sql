080764c4497d419dc27d1d52875da1d85c394ee6
andreupslpornelas <182085@upslp.edu.mx>
2025-11-25 19:13:38 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,5 @@
-
+INSERT INTO empleados (nombre, departamento, salario) VALUES
+('Luis', 'TI', 60000.00),
+('María', 'TI', 62000.00),
+('Juan', 'Recursos Humanos', 55000.00),
+('Ana', 'Marketing', 58000.00);
