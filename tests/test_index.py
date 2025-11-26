8a3a86aa28180266905238eeb5c1eee2db9fc8e1
Palau003 <178561@upslp.edu.mx>
2025-11-25 19:51:42 -0600

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,4 +1,5 @@
-from db import run_query
+from app.db import run_query
+
 
 def test_index_cliente_producto():
     result = run_query("EXPLAIN SELECT * FROM detalle_pedido WHERE id_pedido=1 AND id_producto=2;")
