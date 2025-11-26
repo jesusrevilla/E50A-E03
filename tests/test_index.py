76c7b7004d4add4b810c80f3794f1c7940784adc
miritzatorres8-lab <miritzatorres8@gmail.com>
2025-11-26 14:56:42 -0600

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,1 +1,16 @@
+import psycopg2
+
+def test_index_exists():
+    conn = psycopg2.connect("dbname=test_db user=postgres password=postgres host=localhost")
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT indexname 
+        FROM pg_indexes 
+        WHERE indexname='idx_cliente_producto';
+    """)
+    index = cur.fetchone()
+
+    assert index is not None
+    conn.close()
 
