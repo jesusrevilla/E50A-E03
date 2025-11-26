113c0fc82f14aa7c5c1e43c3bfa8f71c79783ba4
menaxixi <179800@upslp.edu.mx>
2025-11-25 19:45:54 -0600

    Update test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,1 +1,29 @@
+import psycopg2
+import pytest
+
+@pytest.fixture
+def db():
+    conn = psycopg2.connect(
+        host="localhost",
+        port="5432",
+        dbname="test_db",
+        user="postgres",
+        password="postgres"
+    )
+    yield conn
+    conn.close()
+
+
+def test_json_field(db):
+    cur = db.cursor()
+    cur.execute("""
+        INSERT INTO clientes(data)
+        VALUES ('{"nombre": "Luis", "edad": 22}')
+        RETURNING id;
+    """)
+    pk = cur.fetchone()[0]
+    db.commit()
+
+    cur.execute("SELECT data->>'nombre' FROM clientes WHERE id=%s;", (pk,))
+    assert cur.fetchone()[0] == "Luis"
 
