079532a5cc98b41260ae5b97f4e06ec657a951b2
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-11-25 19:16:01 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,24 @@
+CREATE TABLE clientes (
+    id_cliente SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    correo VARCHAR(100)
+);
 
+CREATE TABLE productos (
+    id_producto SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio DECIMAL(10, 2)
+);
+
+CREATE TABLE pedidos (
+    id_pedido SERIAL PRIMARY KEY,
+    id_cliente INT REFERENCES clientes(id_cliente),
+    fecha DATE
+);
+
+CREATE TABLE detalle_pedido (
+    id_detalle SERIAL PRIMARY KEY,
+    id_pedido INT REFERENCES pedidos(id_pedido),
+    id_producto INT REFERENCES productos(id_producto),
+    cantidad INT
+);
