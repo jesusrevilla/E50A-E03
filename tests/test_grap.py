208c07a78f7978d9b653b9b40e32f542b6907480
miritzatorres8-lab <miritzatorres8@gmail.com>
2025-11-26 14:56:24 -0600

    Update test_grap.py

--- tests/test_grap.py
+++ tests/test_grap.py
@@ -1,1 +1,12 @@
+import psycopg2
+
+def test_grafo():
+    conn = psycopg2.connect("dbname=test_db user=postgres password=postgres host=localhost")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM rutas WHERE id_origen=1;")
+    rows = cur.fetchall()
+
+    assert len(rows) >= 1
+    conn.close()
 
