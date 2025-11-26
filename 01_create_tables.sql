8d3b351fd5504e9bc8db10c53ad0cc16806082ba
Yovana-Palencia <174197@upslp.edu.mx>
2025-11-25 19:22:55 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,29 @@
+--Tabla clientes
+CREATE TABLE clientes (
+    id_cliente SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    correo VARCHAR(100)
+);
+
+--Tabla productos
+CREATE TABLE productos (
+    id_producto SERIAL PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio DECIMAL(10, 2)
+);
+
+--Tabla pedidos
+CREATE TABLE pedidos (
+    id_pedido SERIAL PRIMARY KEY,
+    id_cliente INT REFERENCES clientes(id_cliente),
+    fecha DATE
+);
+
+--Tabla detalle_pedido
+CREATE TABLE detalle_pedido (
+    id_detalle SERIAL PRIMARY KEY,
+    id_pedido INT REFERENCES pedidos(id_pedido),
+    id_producto INT REFERENCES productos(id_producto),
+    cantidad INT
+);
 
