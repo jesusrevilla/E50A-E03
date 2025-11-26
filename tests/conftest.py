ca93094cd08c0a9c8853343de5e602153d4e43e2
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-26 00:49:15 -0600

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import os
 
 @pytest.fixture(scope="session")
 def db_conn():
-    """Fixture para establecer y cerrar la conexión a la base de datos."""
     conn = psycopg2.connect(
         host=os.environ.get("DB_HOST", "localhost"),
         user=os.environ.get("DB_USER", "postgres"),
@@ -16,7 +15,6 @@ def db_conn():
 
 @pytest.fixture(scope="function", autouse=True)
 def cursor(db_conn):
-    """Fixture para crear un cursor con ámbito de función y hacer rollback después de cada prueba."""
     cursor = db_conn.cursor()
     yield cursor
     db_conn.rollback()
