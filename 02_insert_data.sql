1c2f9a0f42c47a8cae7f6e3d719656ccae0e5aca
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-25 19:05:18 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,26 +1,21 @@
--- Insertar clientes
+-- Clientes
 INSERT INTO clientes (nombre, correo) VALUES
 ('Ana Torres', 'ana@example.com'),
-('Luis Pérez', 'luis@example.com'),
-('María López', 'maria@example.com');
+('Luis Pérez', 'luis@example.com');
 
--- Insertar productos
+-- Productos
 INSERT INTO productos (nombre, precio) VALUES
 ('Laptop', 1200.00),
 ('Mouse', 25.50),
-('Teclado', 45.00),
-('Monitor', 300.00);
+('Teclado', 45.00);
 
--- Insertar pedidos
+-- Pedidos
 INSERT INTO pedidos (id_cliente, fecha) VALUES
-(1, '2025-05-01'),  -- Ana
-(2, '2025-05-02'),  -- Luis
-(3, '2025-05-03');  -- María
+(1, '2025-05-01'),
+(2, '2025-05-02');
 
--- Insertar detalle de pedidos
+-- Detalle de pedidos
 INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad) VALUES
 (1, 1, 1),  -- Ana compra 1 Laptop
 (1, 2, 2),  -- Ana compra 2 Mouse
-(2, 3, 1),  -- Luis compra 1 Teclado
-(3, 4, 2);  -- María compra 2 Monitores
-
+(2, 3, 1);  -- Luis compra 1 Teclado
