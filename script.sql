c953225db48f99ca990edbd2d39a4947d2d0f91a
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-25 19:20:03 -0600

    feature/182483

--- script.sql
+++ script.sql
@@ -1,1 +1,23 @@
+--------------- 
+
+
+
+
+-------------------------------------------------------------------
+
+SELECT * FROM vista_detalle_pedidos;
+
+CALL registrar_pedido(1, '2025-05-20', 2, 3);
+
+-- Verificar la auditoría
+SELECT * FROM auditoria_pedidos;
+
+SELECT total_gastado_por_cliente(1);
+
+SELECT * FROM productos_json
+WHERE atributos ->> 'marca' = 'Dell';
+
+SELECT nombre, correo
+FROM usuarios
+WHERE historial_actividad @> '[{"accion": "inicio_sesion"}]';
 
