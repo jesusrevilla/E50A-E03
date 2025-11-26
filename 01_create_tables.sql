8abd88309f8de8e0a6ffcc3da58e62eb89838cd0
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-25 19:06:02 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,29 +1,26 @@
--- Tabla de clientes
+
 CREATE TABLE clientes (
     id_cliente SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) UNIQUE NOT NULL
+    nombre VARCHAR(100),
+    correo VARCHAR(100)
 );
 
--- Tabla de productos
 CREATE TABLE productos (
     id_producto SERIAL PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    precio DECIMAL(10,2) NOT NULL
+    nombre VARCHAR(100),
+    precio DECIMAL(10, 2)
 );
 
--- Tabla de pedidos
 CREATE TABLE pedidos (
     id_pedido SERIAL PRIMARY KEY,
-    id_cliente INT NOT NULL REFERENCES clientes(id_cliente),
-    fecha DATE NOT NULL
+    id_cliente INT REFERENCES clientes(id_cliente),
+    fecha DATE
 );
 
--- Tabla de detalle de pedidos
 CREATE TABLE detalle_pedido (
     id_detalle SERIAL PRIMARY KEY,
-    id_pedido INT NOT NULL REFERENCES pedidos(id_pedido),
-    id_producto INT NOT NULL REFERENCES productos(id_producto),
-    cantidad INT NOT NULL CHECK (cantidad > 0)
+    id_pedido INT REFERENCES pedidos(id_pedido),
+    id_producto INT REFERENCES productos(id_producto),
+    cantidad INT
 );
 
