1a20c8d8302c8865049978bbb8e45518e141b5e1
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-26 02:30:46 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,1 +1,16 @@
+import pytest
+import psycopg2
 
+def test_vista_detalle_pedidos():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="exercises",
+        user="postgres",
+        password="postgres"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM vista_detalle_pedidos")
+    results = cur.fetchall()
+    assert len(results) >= 3
+    cur.close()
+    conn.close()
