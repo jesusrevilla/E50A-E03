fedd41e22912c06aa3e4c586e1055a4e5267a8a2
patymtz17 <181730@upslp.edu.mx>
2025-11-25 19:24:16 -0600

    Update 03_retrieve_everything_from_a_table.sql

--- 03_retrieve_everything_from_a_table.sql
+++ 03_retrieve_everything_from_a_table.sql
@@ -1,1 +1,12 @@
-
+-- 1.Vista
+CREATE OR REPLACE VIEW vista_detalle_pedidos AS
+SELECT 
+    p.id_pedido,
+    c.nombre AS cliente,
+    pr.nombre AS producto,
+    dp.cantidad,
+    (pr.precio * dp.cantidad) AS total_linea
+FROM pedidos p
+JOIN clientes c ON p.id_cliente = c.id_cliente
+JOIN detalle_pedido dp ON p.id_pedido = dp.id_pedido
+JOIN productos pr ON dp.id_producto = pr.id_producto;
