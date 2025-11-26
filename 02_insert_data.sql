e728a8900a8fd9ca47a9e8160df60bfe55458679
m00n07 <174653@upslp.edu.mx>
2025-11-25 19:37:43 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,1 +1,51 @@
+-- Clientes (id_cliente: 1, 2)
+INSERT INTO clientes (nombre, correo) VALUES
+('Ana Torres', 'ana@example.com'),
+('Luis Pérez', 'luis@example.com');
 
+-- Productos (id_producto: 1, 2, 3)
+INSERT INTO productos (nombre, precio) VALUES
+('Laptop', 1200.00),
+('Mouse', 25.50),
+('Teclado', 45.00);
+
+-- Pedidos (id_pedido: 1, 2)
+INSERT INTO pedidos (id_cliente, fecha) VALUES
+(1, '2025-05-01'),
+(2, '2025-05-02');
+
+-- Detalle de pedidos
+INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad) VALUES
+(1, 1, 1), -- Ana compra 1 Laptop
+(1, 2, 2), -- Ana compra 2 Mouse
+(2, 3, 1); -- Luis compra 1 Teclado
+
+-- Datos JSONB (productos_json)
+INSERT INTO productos_json (nombre, atributos) VALUES
+('Laptop', '{"marca": "Dell", "ram": "16GB", "procesador": "Intel i7"}'),
+('Smartphone', '{"marca": "Samsung", "pantalla": "6.5 pulgadas", "almacenamiento": "128GB"}'),
+('Tablet', '{"marca": "Apple", "modelo": "iPad Air", "color": "gris"}');
+
+-- Datos JSONB (usuarios)
+INSERT INTO usuarios (nombre, correo, historial_actividad) VALUES
+('Laura Gómez', 'laura@example.com', '[
+    {"fecha": "2025-05-01", "accion": "inicio_sesion"},
+    {"fecha": "2025-05-02", "accion": "subio_archivo"},
+    {"fecha": "2025-05-03", "accion": "cerró_sesion"}
+]'),
+('Pedro Ruiz', 'pedro@example.com', '[
+    {"fecha": "2025-05-01", "accion": "inicio_sesion"},
+    {"fecha": "2025-05-04", "accion": "comentó_publicación"}
+]');
+
+-- Datos Grafos (ciudades) (ID 1:SLP, 2:Querétaro, 3:Guadalajara, 4:Monterrey, 5:CDMX)
+INSERT INTO ciudades (nombre) VALUES
+('San Luis Potosí'), ('Querétaro'), ('Guadalajara'), ('Monterrey'), ('CDMX');
+
+-- Datos Grafos (rutas)
+INSERT INTO rutas (id_origen, id_destino, distancia_km) VALUES
+(1, 2, 180),
+(2, 3, 350),
+(1, 5, 410),
+(5, 4, 900),
+(3, 4, 700);
