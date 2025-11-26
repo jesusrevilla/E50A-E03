14797ff8ef92f54655780143263f58be6f3188ff
Uriel4443 <182829@upslp.edu.mx>
2025-11-25 19:38:11 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -30,3 +30,15 @@ INSERT INTO usuarios (nombre, correo, historial_actividad) VALUES
     {"fecha": "2025-05-01", "accion": "inicio_sesion"},
     {"fecha": "2025-05-04", "accion": "comentó_publicación"}
 ]');
+
+-- Ciudades
+INSERT INTO ciudades (nombre) VALUES
+('San Luis Potosí'), ('Querétaro'), ('Guadalajara'), ('Monterrey'), ('CDMX');
+
+-- Rutas (grafo dirigido)
+INSERT INTO rutas (id_origen, id_destino, distancia_km) VALUES
+(1, 2, 180),  -- SLP → Querétaro
+(2, 3, 350),  -- Querétaro → Guadalajara
+(1, 5, 410),  -- SLP → CDMX
+(5, 4, 900),  -- CDMX → Monterrey
+(3, 4, 700);  -- Guadalajara → Monterrey
