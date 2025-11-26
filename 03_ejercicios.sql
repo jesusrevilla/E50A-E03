f90af3811eefbcb59df9ab864f7d21071a592602
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-25 19:35:03 -0600

    Update 03_ejercicios.sql

--- 03_ejercicios.sql
+++ 03_ejercicios.sql
@@ -74,5 +74,13 @@ FOR EACH ROW
 EXECUTE FUNCTION registrar_auditoria();
 
 --
+--ciudades de san luis
+SELECT c1.nombre AS origen,
+       c2.nombre AS destino,
+       r.distancia_km
+FROM rutas r
+JOIN ciudades c1 ON r.id_origen = c1.id
+JOIN ciudades c2 ON r.id_destino = c2.id
+WHERE c1.nombre = 'San Luis Potosí';
 
 
