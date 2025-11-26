0a202f8ca66dd9c50c78e0311ccd1a0b48e7ab64
Karina123Kma <upslpbd@gmail.com>
2025-11-25 19:38:56 -0600

    Update test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,1 +1,26 @@
+import psycopg2
+import unittest
+
+class TestProductosJson(unittest.TestCase):
+
+    def setUp(self):
+        self.conn = psycopg2.connect(
+            dbname="testdb", user="postgres", password="password", host="localhost", port="5432"
+        )
+        self.cur = self.conn.cursor()
+
+    def tearDown(self):
+        self.cur.close()
+        self.conn.close()
+
+    def test_consultar_producto_por_marca(self):
+        self.cur.execute("""
+            SELECT * FROM productos_json
+            WHERE atributos ->> 'marca' = 'Dell';
+        """)
+        result = self.cur.fetchall()
+        self.assertEqual(len(result), 1)  # Debe haber solo un producto con la marca 'Dell'
+
+if __name__ == '__main__':
+    unittest.main()
 
