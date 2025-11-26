d4b8649fc4d0e68977e219fb58e03b0fe01180a1
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:08:09 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,11 @@
+CREATE VIEW vista_detalle_pedidos AS
+SELECT 
+    c.nombre AS nombre_cliente,
+    p.nombre AS nombre_producto,
+    dp.cantidad,
+    dp.cantidad * p.precio AS total_linea
+FROM detalle_pedido dp
+JOIN pedidos ped ON dp.id_pedido = ped.id_pedido
+JOIN clientes c ON ped.id_cliente = c.id_cliente
+JOIN productos p ON dp.id_producto = p.id_producto;
 
