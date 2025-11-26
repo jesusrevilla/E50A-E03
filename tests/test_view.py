6daceb51c3f2274fd589abdefc7363f9bb622723
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-25 19:21:54 -0600

    Update test_view.py

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,1 +1,11 @@
+import psycopg2
+
+def test_vista_detalle_pedidos():
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost")
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM vista_detalle_pedidos;")
+    filas = cur.fetchall()
+
+    assert len(filas) >= 1
 
