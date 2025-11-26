aa22fcf82545e98bce0820173c6c8e8486a65bd1
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:20:10 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -71,3 +71,8 @@ SELECT nombre, correo
 FROM usuarios
 WHERE historial_actividad @> '[{"accion": "inicio_sesion"}]';
 
+SELECT c1.nombre AS origen, c2.nombre AS destino, r.distancia_km
+FROM rutas r
+JOIN ciudades c1 ON r.id_origen = c1.id
+JOIN ciudades c2 ON r.id_destino = c2.id
+WHERE c1.nombre = 'San Luis Potosí';
