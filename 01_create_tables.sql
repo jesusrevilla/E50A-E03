4a9bb1bae7a9202174a007c014a1262f2b0979ff
AdriannMo <165124296+AdriannMo@users.noreply.github.com>
2025-11-25 19:19:00 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,23 @@
+DROP TABLE IF EXISTS rutas;
+DROP TABLE IF EXISTS ciudades;
+DROP TABLE IF EXISTS productos;
 
+-- Tabla para ejercicio de Arrays
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT,
+    etiquetas TEXT[] -- Array de texto
+);
+
+-- Tablas para ejercicio de Grafos
+CREATE TABLE ciudades (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL
+);
+
+CREATE TABLE rutas (
+    id SERIAL PRIMARY KEY,
+    id_origen INT REFERENCES ciudades(id),
+    id_destino INT REFERENCES ciudades(id),
+    distancia_km INT
+);
