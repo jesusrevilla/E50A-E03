a87da52cfa4c1bcd8cb063c4413d8b1fff05ef36
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-26 04:17:11 -0600

    Create conftest.py

---
+++ tests/Update and rename tests/tests/conftest.py to tests/conftest.py
@@ -0,0 +1,29 @@
+import psycopg2
+import pytest
+
+@pytest.fixture(scope="module")
+def db_connection():
+    """
+    Fixture de pytest para establecer una conexión de módulo a la base de datos de prueba.
+    """
+    try:
+        
+        conn = psycopg2.connect(
+            host="localhost",
+            database="test_db",
+            user="postgres",
+            password="postgres"
+        )
+       
+        conn.autocommit = True
+
+    except psycopg2.Error as e:
+        pytest.fail(f"Fallo al conectar con PostgreSQL: {e}")
+        return
+
+    
+    yield conn
+
+    
+    if conn:
+        conn.close()
