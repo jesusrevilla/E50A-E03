4f1cb3b79152ccb93226a84ac2f1d6f59dbf381a
Fano-Rdz <178584@upslp.edu.mx>
2025-11-25 19:34:16 -0600

    Update test_json.py

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,1 +1,9 @@
+from db import run_query
 
+def test_productos_json():
+    result = run_query("SELECT * FROM productos_json WHERE atributos ->> 'marca' = 'Dell';")
+    assert len(result) > 0
+
+def test_usuarios_json():
+    result = run_query("SELECT nombre FROM usuarios WHERE historial_actividad @> '[{\"accion\": \"inicio_sesion\"}]';")
+    assert len(result) > 0
