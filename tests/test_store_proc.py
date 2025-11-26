f40f53da3676064d980a1e316c788a2176b21aa7
daf01-web <171513@upslp.edu.mx>
2025-11-25 20:00:25 -0600

    Update test_store_proc.py

--- tests/test_store_proc.py
+++ tests/test_store_proc.py
@@ -1,36 +1,9 @@
 
-import psycopg2
-
-from .conftest import db_connection 
-
-def test_registrar_pedido_exitoso():
-    conn = db_connection()
+def test_store_proc(conn):
     cur = conn.cursor()
-    
-    id_cliente_prueba = 1
-    fecha_prueba = '2025-12-01'
-    id_producto_prueba = 2  # Mouse
-    cantidad_prueba = 5
+    cur.execute("CALL registrar_pedido(1, '2025-05-20', 1, 2);")
+    conn.commit()
 
-    cur.execute(f"SELECT registrar_pedido({id_cliente_prueba}, '{fecha_prueba}', {id_producto_prueba}, {cantidad_prueba});")
-    
-    
-    cur.execute("SELECT id_pedido, id_cliente, fecha FROM pedidos ORDER BY id_pedido DESC LIMIT 1;")
-    new_pedido = cur.fetchone()
-    
-    assert new_pedido is not None
-    assert new_pedido[1] == id_cliente_prueba
+    cur.execute("SELECT * FROM pedidos ORDER BY id_pedido DESC LIMIT 1;")
+    assert cur.fetchone() is not None
 
-    new_pedido_id = new_pedido[0]
-
-   
-    cur.execute("SELECT id_producto, cantidad FROM detalle_pedido WHERE id_pedido = %s;", (new_pedido_id,))
-    detalle = cur.fetchone()
-    
-    assert detalle is not None
-    assert detalle[0] == id_producto_prueba
-    assert detalle[1] == cantidad_prueba
-
-    conn.rollback() # Revertir cambios después de la prueba
-    cur.close()
-    conn.close()
