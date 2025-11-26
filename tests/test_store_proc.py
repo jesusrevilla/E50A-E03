fe0a270ea4a53bada345ec8650af6f2058da0490
patymtz17 <181730@upslp.edu.mx>
2025-11-25 19:36:30 -0600

    Update test_store_proc.py

--- tests/test_store_proc.py
+++ tests/test_store_proc.py
@@ -1,1 +1,8 @@
+def test_store_proc(conn):
+    cur = conn.cursor()
+    cur.execute("CALL registrar_pedido(1, '2025-05-20', 1, 2);")
+    conn.commit()
+
+    cur.execute("SELECT * FROM pedidos ORDER BY id_pedido DESC LIMIT 1;")
+    assert cur.fetchone() is not None
 
