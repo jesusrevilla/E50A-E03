d8dc88b77783045236ea6ba1e46be953a30641a6
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-11-25 19:30:36 -0600

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,1 +1,6 @@
+from db import run_query
 
+def test_funciones_adicionales():
+    # Puedes poner aquí pruebas de otras funciones si existieran
+    result = run_query("SELECT total_gastado_por_cliente(1);")
+    assert result[0][0] >= 0
