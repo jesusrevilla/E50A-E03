201b4af76a8a9678785e594d429040922a24c0c2
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:34:51 -0600

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -14,3 +14,14 @@ def run_query(query):
         with conn.cursor() as cur:
             cur.execute(query)
             return cur.fetchall()
+
+def test_total_gastado_por_cliente():
+    """La función 'total_gastado_por_cliente' debe devolver el total gastado correctamente."""
+    query = """
+        SELECT total_gastado_por_cliente(1);
+    """
+    result = run_query(query)
+
+    expected_total = 1200.00 + 51.00
+
+    assert result[0][0] == expected_total
