ad9cc3d9b226972d858fa2fed7ed1f3cb2588e1a
Uriel4443 <182829@upslp.edu.mx>
2025-11-25 19:10:21 -0600

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,1 +1,12 @@
-
+CREATE VIEW vista_detalle_pedidos AS
+SELECT 
+    p.id_pedido,
+    c.nombre AS cliente,
+    pr.nombre AS producto,
+    d.cantidad,
+    pr.precio,
+    (d.cantidad * pr.precio) AS total_linea
+FROM detalle_pedido d
+JOIN pedidos p ON d.id_pedido = p.id_pedido
+JOIN clientes c ON p.id_cliente = c.id_cliente
+JOIN productos pr ON d.id_producto = pr.id_producto;
