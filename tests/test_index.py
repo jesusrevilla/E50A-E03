c792d2df67a6e462d4e3137eb420adc74110a8e5
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-25 19:47:52 -0600

    feature/182483

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,1 +1,24 @@
+import psycopg2
+import pytest
+
+DB = {
+    "dbname": "test_db",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "localhost",
+    "port": 5432
+}
+
+def test_index_exists():
+    conn = psycopg2.connect(**DB)
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT indexname
+        FROM pg_indexes
+        WHERE indexname = 'idx_cliente_producto';
+    """)
+    index = cur.fetchone()
+    assert index is not None  #checa que el indice si debe existir
+    conn.close()
 
