62d659ba7b167a71e155b78fa85121fb53991b35
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-25 19:47:07 -0600

    Update 08_graphs.sql

--- 08_graphs.sql
+++ 08_graphs.sql
@@ -18,3 +18,10 @@ INSERT INTO ciudades (nombre) VALUES
 
 INSERT INTO rutas (id_origen, id_destino, distancia_km) VALUES
 (1,2,180), (2,3,350), (1,5,410), (5,4,900), (3,4,700);
+
+-- Ver todas las rutas desde San Luis Potosí
+SELECT c1.nombre AS origen, c2.nombre AS destino, r.distancia_km
+FROM rutas r
+JOIN ciudades c1 ON r.id_origen = c1.id
+JOIN ciudades c2 ON r.id_destino = c2.id
+WHERE c1.nombre = 'San Luis Potosí';
