ded6a562ece5e3c2e635a8c745ced500e6f58d9d
Fano-Rdz <178584@upslp.edu.mx>
2025-11-25 19:42:19 -0600

    Update 03_functions_and_procedures.sql

--- 03_functions_and_procedures.sql
+++ 03_functions_and_procedures.sql
@@ -1,1 +1,34 @@
+--Stored Procedure
+CREATE OR REPLACE PROCEDURE registrar_pedido(
+    p_cliente INT,
+    p_fecha DATE,
+    p_producto INT,
+    p_cantidad INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    new_pedido INT;
+BEGIN
+    INSERT INTO pedidos(id_cliente, fecha) VALUES (p_cliente, p_fecha) RETURNING id_pedido INTO new_pedido;
+    INSERT INTO detalle_pedido(id_pedido, id_producto, cantidad) VALUES (new_pedido, p_producto, p_cantidad);
+END;
+$$;
 
+-- Función
+CREATE OR REPLACE FUNCTION total_gastado_por_cliente(p_cliente INT)
+RETURNS NUMERIC
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    total NUMERIC;
+BEGIN
+    SELECT SUM(d.cantidad * pr.precio) INTO total
+    FROM pedidos pd
+    JOIN detalle_pedido d ON pd.id_pedido = d.id_pedido
+    JOIN productos pr ON d.id_producto = pr.id_producto
+    WHERE pd.id_cliente = p_cliente;
+
+    RETURN COALESCE(total, 0);
+END;
+$$;
