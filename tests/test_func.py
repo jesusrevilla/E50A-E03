f32d54c7bbccbc94a6528f42eda63cfc7911fff6
AdriannMo <165124296+AdriannMo@users.noreply.github.com>
2025-11-25 19:31:13 -0600

    Update test_func.py

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,1 +1,16 @@
+import psycopg2
+import pytest
 
+DB_CONFIG = { "dbname": "test_db", "user": "postgres", "password": "postgres", "host": "localhost", "port": 5432 }
+
+@pytest.fixture(scope="module")
+def db_connection():
+    conn = psycopg2.connect(**DB_CONFIG)
+    yield conn
+    conn.close()
+
+def test_func_total_gastado(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT total_gastado_por_cliente(1);")
+        result = cur.fetchone()[0]
+        assert float(result) == 1251.00
