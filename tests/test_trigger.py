57c704e15f12b19d62a6b69bed6494e368c79b13
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:35:29 -0600

    Update test_trigger.py

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -14,3 +14,16 @@ def run_query(query):
         with conn.cursor() as cur:
             cur.execute(query)
             return cur.fetchall()
+
+def test_trigger_auditoria_pedido():
+    query_insert = """
+        INSERT INTO pedidos (id_cliente, fecha) VALUES (2, '2025-05-15');
+    """
+    run_query(query_insert)
+
+    query_auditoria = """
+        SELECT * FROM auditoria_pedidos WHERE id_cliente = 2 AND fecha_pedido = '2025-05-15';
+    """
+    auditoria_result = run_query(query_auditoria)
+
+    assert len(auditoria_result) == 1
