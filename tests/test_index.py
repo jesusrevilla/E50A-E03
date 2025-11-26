6d3f29b65a3d777574c908489047e4022f35b476
FernandaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2025-11-25 19:38:04 -0600

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -14,3 +14,15 @@ def run_query(query):
         with conn.cursor() as cur:
             cur.execute(query)
             return cur.fetchall()
+            
+def test_indice_cliente_producto():
+    """Verifica que el índice compuesto 'idx_cliente_producto' exista en la tabla 'detalle_pedido'."""
+    query = """
+        SELECT indexname
+        FROM pg_indexes
+        WHERE tablename = 'detalle_pedido' AND indexname = 'idx_cliente_producto';
+    """
+    result = run_query(query)
+
+    assert len(result) == 1
+    assert result[0][0] == 'idx_cliente_producto'
