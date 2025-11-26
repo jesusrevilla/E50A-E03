8fba1a18a275a726de18163b6e0769a9317f06f4
Javierxd1383 <javiercruzz1383@gmail.com>
2025-11-25 19:21:41 -0600

    Update test_trigger.py

--- tests/test_trigger.py
+++ tests/test_trigger.py
@@ -1,1 +1,18 @@
+import psycopg2
+
+def test_trigger_auditoria():
+    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres host=localhost")
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO pedidos (id_cliente, fecha) VALUES (1, '2025-06-01');")
+    conn.commit()
+
+    cur.execute("""
+        SELECT COUNT(*) 
+        FROM auditoria_pedidos 
+        WHERE fecha_pedido='2025-06-01';
+    """)
+    
+    registros = cur.fetchone()[0]
+    assert registros >= 1
 
