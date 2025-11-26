55a7a11038ae5a7fba64d5566034dad826d442e4
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-26 02:31:44 -0600

    Update test_index.py

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,1 +1,20 @@
+import pytest
+import psycopg2
 
+def test_index_exists():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="exercises",
+        user="postgres",
+        password="postgres"
+    )
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT COUNT(*) 
+        FROM pg_indexes 
+        WHERE indexname = 'idx_cliente_producto'
+    """)
+    exists = cur.fetchone()[0]
+    assert exists == 1
+    cur.close()
+    conn.close()
