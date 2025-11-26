1b7cc0ba65fafd29d32aef95d701809a80a8b358
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-11-25 19:12:59 -0600

    Update 03_generar.sql

--- 03_generar.sql
+++ 03_generar.sql
@@ -1,3 +1,4 @@
+-- Vista
 CREATE OR REPLACE VIEW vista_detalle_pedidos AS
 SELECT 
     c.nombre AS nombre_cliente,
@@ -13,3 +14,28 @@ FROM
     JOIN productos p ON dp.id_producto = p.id_producto;
     
 SELECT * FROM vista_detalle_pedidos;
+
+-- Procedimiento almacenado
+CREATE OR REPLACE PROCEDURE registrar_pedido(
+    p_id_cliente INT,
+    p_fecha DATE,
+    p_id_producto INT,
+    p_cantidad INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_id_pedido INT;
+BEGIN
+    INSERT INTO pedidos (id_cliente, fecha)
+    VALUES (p_id_cliente, p_fecha)
+    RETURNING id_pedido INTO v_id_pedido;
+
+    INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad)
+    VALUES (v_id_pedido, p_id_producto, p_cantidad);
+    
+    RAISE NOTICE 'Pedido registrado correctamente';
+END;
+$$;
+
+CALL registrar_pedido(1, '2025-05-20', 2, 3);
