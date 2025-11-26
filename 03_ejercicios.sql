b2d4c2546013ca32fb2b73b78ffdd0ad4f892974
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-25 19:23:08 -0600

    Create 03_ejercicios.sql

---
+++ 03_ejercicios.sql
@@ -0,0 +1,78 @@
+--vista
+CREATE VIEW vista_detalle_pedidos AS
+SELECT 
+    p.id_pedido,
+    c.nombre AS nombre_cliente,
+    pr.nombre AS nombre_producto,
+    dp.cantidad,
+    pr.precio,
+    (dp.cantidad * pr.precio) AS total_linea,
+    p.fecha
+FROM detalle_pedido dp
+JOIN pedidos p ON dp.id_pedido = p.id_pedido
+JOIN clientes c ON p.id_cliente = c.id_cliente
+JOIN productos pr ON dp.id_producto = pr.id_producto;
+
+--procedimiento
+CREATE OR REPLACE PROCEDURE registrar_pedido(
+    p_id_cliente INT,
+    p_fecha DATE,
+    p_id_producto INT,
+    p_cantidad INT
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_id_pedido INT;
+BEGIN
+    INSERT INTO pedidos (id_cliente, fecha)
+    VALUES (p_id_cliente, p_fecha)
+    RETURNING id_pedido INTO v_id_pedido;
+
+    INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad)
+    VALUES (v_id_pedido, p_id_producto, p_cantidad);
+
+    RAISE NOTICE 'Pedido registrado con ID: %', v_id_pedido;
+END;
+$$;
+
+--funcion
+CREATE OR REPLACE FUNCTION total_gastado_por_cliente(p_id_cliente INT)
+RETURNS DECIMAL(10,2)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    v_total DECIMAL(10,2);
+BEGIN
+    SELECT COALESCE(SUM(dp.cantidad * pr.precio), 0)
+    INTO v_total
+    FROM pedidos p
+    JOIN detalle_pedido dp ON p.id_pedido = dp.id_pedido
+    JOIN productos pr ON dp.id_producto = pr.id_producto
+    WHERE p.id_cliente = p_id_cliente;
+
+    RETURN v_total;
+END;
+$$;
+
+--INDICe
+CREATE INDEX idx_cliente_producto ON detalle_pedido (id_cliente, id_producto);
+
+--trigger
+CREATE OR REPLACE FUNCTION registrar_auditoria()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO auditoria_pedidos (id_cliente, fecha_pedido)
+    VALUES (NEW.id_cliente, NEW.fecha);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_auditoria_pedidos
+AFTER INSERT ON pedidos
+FOR EACH ROW
+EXECUTE FUNCTION registrar_auditoria();
+
+--
+
+
