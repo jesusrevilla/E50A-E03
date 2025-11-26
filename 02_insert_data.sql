c59d505bad007470f0586e7d021fc95697f98aca
Jpablo179804 <179804@upslp.edu.mx>
2025-11-25 19:39:42 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,20 +1,24 @@
+-- Clientes
 INSERT INTO clientes (nombre, correo) VALUES
 ('Ana Torres', 'ana@example.com'),
 ('Luis Pérez', 'luis@example.com');
 
+-- Productos
 INSERT INTO productos (nombre, precio) VALUES
 ('Laptop', 1200.00),
 ('Mouse', 25.50),
 ('Teclado', 45.00);
 
+-- Pedidos
 INSERT INTO pedidos (id_cliente, fecha) VALUES
 (1, '2025-05-01'),
 (2, '2025-05-02');
 
+-- Detalle de pedidos
 INSERT INTO detalle_pedido (id_pedido, id_producto, cantidad) VALUES
-(1, 1, 1), 
-(1, 2, 2), 
-(2, 3, 1); 
+(1, 1, 1),  -- Ana compra 1 Laptop
+(1, 2, 2),  -- Ana compra 2 Mouse
+(2, 3, 1);  -- Luis compra 1 Teclado
 
 INSERT INTO productos_json (nombre, atributos) VALUES
 ('Laptop', '{"marca": "Dell", "ram": "16GB", "procesador": "Intel i7"}'),
