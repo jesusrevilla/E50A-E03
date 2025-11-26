535ea06fd69f6babde4c160f90d42c16a1542806
menaxixi <179800@upslp.edu.mx>
2025-11-25 19:52:56 -0600

    Update test_grap.py

--- tests/test_grap.py
+++ tests/test_grap.py
@@ -1,64 +1,40 @@
 import psycopg2
-import pytest
 
-@pytest.fixture
-def db():
+def test_insert_row():
     conn = psycopg2.connect(
-        host="localhost",
-        port="5432",
-        dbname="test_db",
-        user="postgres",
-        password="postgres"
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port=5432
     )
-    yield conn
-    conn.rollback()
-    conn.close()
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO clientes(nombre) VALUES ('pato') RETURNING id;")
+    new_id = cur.fetchone()[0]
+    conn.commit()
+
+    cur.execute("SELECT nombre FROM clientes WHERE id = %s;", (new_id,))
+    result = cur.fetchone()[0]
+    assert result == 'pato'
 
 
-def test_insert(db):
-    cur = db.cursor()
-    cur.execute("""
-        INSERT INTO productos(nombre, precio)
-        VALUES ('Test Cola', 11)
-        RETURNING id;
-    """)
-    pk = cur.fetchone()[0]
-    db.commit()
+def test_update_row():
+    conn = psycopg2.connect(
+        dbname='test_db',
+        user='postgres',
+        password='postgres',
+        host='localhost',
+        port=5432
+    )
+    cur = conn.cursor()
 
-    cur.execute("SELECT nombre FROM productos WHERE id=%s;", (pk,))
-    assert cur.fetchone()[0] == "Test Cola"
+    cur.execute("INSERT INTO clientes(nombre) VALUES ('viejo') RETURNING id;")
+    cid = cur.fetchone()[0]
 
+    cur.execute("UPDATE clientes SET nombre = 'nuevo' WHERE id = %s;", (cid,))
+    conn.commit()
 
-def test_update(db):
-    cur = db.cursor()
-    cur.execute("""
-        INSERT INTO productos(nombre, precio)
-        VALUES ('Test Gansito', 20)
-        RETURNING id;
-    """)
-    pk = cur.fetchone()[0]
-    db.commit()
-
-    cur.execute("UPDATE productos SET precio=25 WHERE id=%s;", (pk,))
-    db.commit()
-
-    cur.execute("SELECT precio FROM productos WHERE id=%s;", (pk,))
-    assert cur.fetchone()[0] == 25
-
-
-def test_delete(db):
-    cur = db.cursor()
-    cur.execute("""
-        INSERT INTO productos(nombre, precio)
-        VALUES ('Test Sprite', 10)
-        RETURNING id;
-    """)
-    pk = cur.fetchone()[0]
-    db.commit()
-
-    cur.execute("DELETE FROM productos WHERE id=%s;", (pk,))
-    db.commit()
-
-    cur.execute("SELECT * FROM productos WHERE id=%s;", (pk,))
-    assert cur.fetchone() is None
-
+    cur.execute("SELECT nombre FROM clientes WHERE id = %s;", (cid,))
+    result = cur.fetchone()[0]
+    assert result == 'nuevo'
