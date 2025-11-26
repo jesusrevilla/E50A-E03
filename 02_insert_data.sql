e4989fcfc6021adb3705c34ccb551cd66b706bee
Karina123Kma <upslpbd@gmail.com>
2025-11-25 19:31:02 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,5 +1,1 @@
-INSERT INTO productos_json (nombre, atributos) VALUES
-('Laptop', '{"marca": "Dell", "ram": "16GB", "procesador": "Intel i7"}'),
-('Smartphone', '{"marca": "Samsung", "pantalla": "6.5 pulgadas", "almacenamiento": "128GB"}'),
-('Tablet', '{"marca": "Apple", "modelo": "iPad Air", "color": "gris"}');
 
